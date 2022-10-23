7ca9a1a2955cf389b8b21d0c092cc05742dde948
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-23 01:11:20 -0700

    show stylesheet

--- server/app.js
+++ server/app.js
@@ -4,7 +4,8 @@ const app = express();
 // app.use(express.static(__dirname + '/public'));
 // app.use(express.static(__dirname + '/assets'));
 
-app.use('/scripts', express.static('assets/scripts'));
+// app.use('/scripts', express.static('assets/scripts'));
+app.use('/stylesheets', express.static('assets/css'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
