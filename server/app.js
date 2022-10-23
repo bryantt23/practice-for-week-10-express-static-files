b3f2a315b6f86005f6e7d02e2c46b2a0ec12a2f9
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-23 00:55:36 -0700

    serve assets

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 // Your code here
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/assets'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
