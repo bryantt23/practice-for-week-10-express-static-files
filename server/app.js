6b387285cae5f2e26af76ed191e776ae40aea03e
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-23 01:01:49 -0700

    can get all 3 files

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 // Your code here
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/assets'));
+// app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/assets'));
+
+app.use('/', express.static('assets'));
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
